425224b487354501274a3c59abfc12267ce3d6f1
DevilMortar <devilmortar0159@gmail.com>
2023-04-21 14:51:13 +0900

    new JS script

---
+++ www/js/script.js
@@ -0,0 +1,10 @@
+//Display the date and time in the div with id "time"
+function displayTime() {
+    var time = document.getElementById("time");
+    var date = new Date();
+    time.innerHTML = date.toLocaleString();
+}
+
+// Call the function every second
+setInterval(displayTime, 1000);
+
